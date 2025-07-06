bf42ea5dc2b8c1be2070e86758a57c7face6a711
amin-97 <amin.khan2897@gmail.com>
2025-07-06 19:57:59 -0400

    routes setup

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>Companions New</div>;
+};
+
+export default Page;
---
+++ app/companions/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>Companions</div>;
+};
+
+export default Page;
