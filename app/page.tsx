81375cedc1edfacd4fd4181a7a6f6825ecd701b6
amin-97 <amin.khan2897@gmail.com>
2025-07-06 21:44:26 -0400

    implemented more files

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,40 @@
 import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import CTA from "@/components/CTA";
-import { Button } from "@/components/ui/button";
-import React from "react";
+import { recentSessions } from "@/constants";
+import {
+  getAllCompanions,
+  getRecentSessions,
+} from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
 
-const Page = () => {
+const Page = async () => {
+  const companions = await getAllCompanions({ limit: 3 });
+  const recentSessionsCompanions = await getRecentSessions(10);
+
   return (
-    <div>
-      <h1 className="text-2xl underline">Popular Companions</h1>
+    <main>
+      <h1>Popular Companions</h1>
 
-      <section>
-        <CompanionCard
-          id="123"
-          name="Neura the Brainy Explorer"
-          topic="Neural Network of the Brain"
-          subject="science"
-          duration={30}
-          color="#ffda6e"
-        />
-        <CompanionCard
-          id="456"
-          name="Countsy the Number Wizard"
-          topic="Derivatives & Integrals"
-          subject="science"
-          duration={30}
-          color="#e5d0ff"
-        />
-        <CompanionCard
-          id="789"
-          name="Verba the Vocab Builder"
-          topic="language"
-          subject="English Literature"
-          duration={30}
-          color="#BDE7FF"
-        />
+      <section className="home-section">
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
       </section>
 
       <section className="home-section">
-        <CompanionsList />
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessionsCompanions}
+          classNames="w-2/3 max-lg:w-full"
+        />
         <CTA />
       </section>
-    </div>
+    </main>
   );
 };
 
